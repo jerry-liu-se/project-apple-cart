68c597f7a6e437f0fb4754ebea2f38de7524bcf7
jerry-liu1 <jerry.liu1@se.com>
2024-06-17 18:23:08 -0700

    added search bar with css

--- apple_cart.py
+++ apple_cart.py
@@ -24,11 +24,12 @@ def load_saturn_db(db_path: pathlib.Path, enum_path: pathlib.Path):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    db_path = r"C:\Users\SESA675699\CODE\Libra\saturn\test\lib_python\saturndb\json\saturn_sysvarDB.json"
-    enum_path = r"C:\Users\SESA675699\CODE\Libra\saturn\test\lib_python\saturndb\json\saturn_enums.json"
-    start_time = time.time()
-    saturn_db, enum_db = load_saturn_db(Path(db_path), Path(enum_path))
-    print(time.time() - start_time)
+
+    # db_path = r"C:\Users\SESA675699\CODE\Libra\saturn\test\lib_python\saturndb\json\saturn_sysvarDB.json"
+    # enum_path = r"C:\Users\SESA675699\CODE\Libra\saturn\test\lib_python\saturndb\json\saturn_enums.json"
+    # start_time = time.time()
+    # saturn_db, enum_db = load_saturn_db(Path(db_path), Path(enum_path))
+    # print(time.time() - start_time)
 
     if request.method == 'POST':
         text = request.form.get('textarea')
